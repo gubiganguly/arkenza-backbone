2b3f48ef39cb2674c94b21a810bb2ed4efc1284d
Jason Stillerman <jason.t.stillerman@gmail.com>
2025-01-28 19:57:34 -0500

    moving stuff around

--- app/introduction/page.tsx
+++ app/introduction/page.tsx
@@ -1,7 +1,6 @@
-import { SlideDeck } from "@/components/slide-deck"
-import { introductionModule } from "@/data/modules/introduction"
+import { SlideDeck } from "@/components/slide-deck";
+import { introductionModule } from "@/app/introduction/introduction";
 
 export default function IntroductionPage() {
-  return <SlideDeck module={introductionModule} />
+  return <SlideDeck module={introductionModule} />;
 }
-
